fe75f923a2f897718ac19f7c61c7dad6fe83c836
Ansari-cmd <133573011+Ansari-cmd@users.noreply.github.com>
2024-12-03 23:50:25 +0530

    Add files via upload

---
+++ Portfolio/js/script.js
@@ -0,0 +1,15 @@
+/* ============================ typing  animation*/
+var typed = new Typed(".typing", {
+  strings: [
+    "Python Developer",
+    "Django Developer",
+    "SQL Developer",
+    "Web Developer",
+    "Web Designer",
+    "Backend Developer",
+    "Graphic Designer",
+  ],
+  typedSpeed: 100,
+  BackSpeed: 20,
+  loop: true,
+});
